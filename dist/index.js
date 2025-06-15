a28ef04f2a140874bffe2cdddc5d4737fbc2aa89
Kshitiz-alt <saxenaktz@gmail.com>
2025-06-15 17:24:38 +0530

    backend production in the process...14

--- dist/index.js
+++ dist/index.js
@@ -8,9 +8,14 @@ require("dotenv/config");
 const cors_1 = __importDefault(require("cors"));
 const zipFold_1 = __importDefault(require("./routes/zipFold"));
 const app = (0, express_1.default)();
-app.use((0, cors_1.default)());
+app.use((0, cors_1.default)({
+    origin: process.env.FRONTEND_URL,
+    methods: ['GET', 'POST'],
+    credentials: true
+}));
+const port = process.env.PORT || 5000;
 app.use(express_1.default.json());
 // Register the ZIP route here
 app.use(zipFold_1.default);
 // Other routes, static files, etc.
-app.listen(process.env.PORT, () => console.log(`Server running on port:${process.env.PORT}`));
+app.listen(port, () => console.log(`Server running on port:${port}`));
