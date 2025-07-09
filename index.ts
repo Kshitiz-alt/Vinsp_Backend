f2ba6bfb494d4b9963e82b7a59c3b26f687053bd
Kshitiz-alt <saxenaktz@gmail.com>
2025-07-09 21:10:55 +0530

    done

--- index.ts
+++ index.ts
@@ -10,11 +10,18 @@ import ArtistRoute from './routes/ArtistsRoutes';
 
 const app = express();
 
+const allowedOrigins = process.env.FRONTEND_URLS?.split(',') || []
 
 app.use(cors({
-    origin: process.env.FRONTEND_URL,
-    methods: ['GET', 'POST'],
-    credentials: true
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  methods: ['GET', 'POST'],
+  credentials: true
 }));
 // app.use(cors())
 
