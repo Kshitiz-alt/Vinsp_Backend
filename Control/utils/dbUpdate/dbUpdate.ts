69bca14a797832a0c315eac9fba60924521b891b
Kshitiz-alt <saxenaktz@gmail.com>
2025-07-26 13:38:33 +0530

    safe key applied

--- Control/utils/dbUpdate/dbUpdate.ts
+++ Control/utils/dbUpdate/dbUpdate.ts
@@ -9,10 +9,10 @@ async function stripBaseUrls() {
   try {
 
     // Update `audio`
-    await pool.query(
-      `UPDATE artists SET audio = REPLACE(audio, $1, '') WHERE audio LIKE $2`,
-      [oldBaseUrl, `${oldBaseUrl}%`]
-    );
+    // await pool.query(
+    //   `UPDATE artists SET audio = REPLACE(audio, $1, '') WHERE audio LIKE $2`,
+    //   [oldBaseUrl, `${oldBaseUrl}%`]
+    // );
 
     // Update `image`
     await pool.query(
@@ -23,10 +23,41 @@ async function stripBaseUrls() {
     console.log('Successfully stripped base URLs from audio and image.');
   } catch (err) {
     console.error('Failed to update URLs:', err);
-  } finally {
-    await pool.end();
   }
+};
+
+
+async function updatedBunnyUrl() {
+  try {
+
+    await pool.query(`
+       UPDATE albums SET image = '/images/theweekend/The%20Weeknd.jpg' 
+         WHERE image = '/images/The%20Weeknd.jpg'
+      `);
+
+    await pool.query(
+      `UPDATE albums SET image = '/images/theweekend/Gone%20(Explicit).jpg' 
+         WHERE image = '/images/Gone%20(Explicit).jpg'`
+    );
+
+    await pool.query(
+      `UPDATE songs 
+   SET image = REPLACE(image, 'images/theweekend/The%20Weeknd.jpg','images/theweekend/Gone%20(Explicit).jpg') 
+   WHERE title =  'Gone'`
+    );
+    console.log("successfully added folders")
+
+  } catch (err) {
+    console.error("error while replacing updatedUrl", err)
+  } finally {
+    await pool.end()
+  }
+};
+
+async function main() {
+  await stripBaseUrls()
+  await updatedBunnyUrl()
 }
 
-stripBaseUrls();
+main()
 
