a24b054dd96ed590f8b6a9f86ce24059fada5f76
Kshitiz-alt <saxenaktz@gmail.com>
2025-08-10 14:37:13 +0530

    added new data

--- Control/utils/dbUpdate/dbUpdate.ts
+++ Control/utils/dbUpdate/dbUpdate.ts
@@ -1,38 +1,31 @@
-import { pool } from "../db";
-import songData from '../../../metadata/songs.json';
-import dotenv from 'dotenv';
-dotenv.config();
+// import { pool } from "../db";
+// import songData from '../../../metadata/songs.json';
+// import dotenv from 'dotenv';
+// dotenv.config();
 
-const songs = songData.song.filter(
-  (song) => song.artist.trim().toLowerCase() === "the weeknd"
-);
-
-function slugify(text: string) {
-  return text.toLowerCase().replace(/\s+/g, '').replace(/[^a-z0-9]/g, '');
-}
-
-function sanitizeFilename(text: string) {
-  return encodeURIComponent(text.replace(/[\/\\?%*:|"<>]/g, '').trim());
-}
-
-async function stripBaseUrls() {
-  try {
-    for (const song of songs) {
-      console.log(`Updating ID ${song.id} with audio: ${song.audio}`);
-
-      await pool.query(
-        `UPDATE songs SET audio = $1 WHERE id = $2`,
-        [song.audio, song.id]
-      );
-    }
-
-    console.log('Successfully updated audio & image URLs.');
-  } catch (err) {
-    console.error('Failed to update URLs:', err);
-  } finally {
-    await pool.end();
-  }
-}
+// const songs = songData.song.filter(s => s.id >= 338 && s.id <= 353);
 
 
-stripBaseUrls();
+// async function stripBaseUrls() {
+//   try {
+//     for (const song of songs) {
+//       console.log(`inserting ${song.id} with images:${song.image} & audio: ${song.audio}`);
+
+//       await pool.query(
+//          `INSERT INTO songs (id, title, artist, artistid, albumid, image, audio, duration, language, lyrics)
+//        VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10)
+//        ON CONFLICT (id) DO NOTHING`,
+//         [song.id,song.title,song.artist,song.artistId,song.albumId,song.image,song.audio,song.duration,song.language,song.lyrics]
+//       );
+//     }
+
+//     console.log('Successfull');
+//   } catch (err) {
+//     console.error('Failed to insert URLs:', err);
+//   } finally {
+//     await pool.end();
+//   }
+// }
+
+
+// stripBaseUrls();
